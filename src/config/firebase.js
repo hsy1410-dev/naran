1c70288f2b0e6cacb38d6ecd1dbf20d6fa8ec7c8
hsy1410-dev <hsy1410@gmail.com>
2025-12-01 16:55:03 +0900

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -6,7 +6,7 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MSG_ID,
